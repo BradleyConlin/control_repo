389c403a71a9d4b26b7ccd7073a1374e4d9d23f8
Brad Conlin <65742142+BradleyConlin@users.noreply.github.com>
2021-10-01 11:40:54 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDIOckfC907EeZyLIVevuPdS/8dxzciwT5wJS0DIySwdNFhLGYFWGNfTXSjdu+A2HAtePKmmIgTpFu5SfQrSyWOfwHBlPtL238/r2LY7olxmBr1u06alXpVpAhNXfCy9mBvtxnSG7Aer0Jxy3jzyXsqCGx3ih2sRVnF7yccAS0zS8QbX1TI6H7MRJp9UxeeUha5Wzofc0a/v2fg3P/TYH0KEF2QSAX+FKwjYdy3W3nD/Nf8hgAGJsmv83pCTWxsUr+q6tIlr4YCVV2K299rcZxdxZOSPmzPYUsiGAWxTD5U0N1osmtpbcerczCHP8iO9WHN6emvY0taDf1a1ARc08Qt',
 	}  
 }
