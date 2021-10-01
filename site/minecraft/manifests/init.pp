414e3ec326a4f48943043b0fb0f600df0272d5dc
Brad Conlin <65742142+BradleyConlin@users.noreply.github.com>
2021-10-01 12:15:25 -0400

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,21 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.jar':
+    ensure => file,
+    source => 'http://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.text':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service'
+  }
+  
+}
